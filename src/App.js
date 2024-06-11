b8e63ed1585d77ac608bbccd1dab196bb727472e
CYTech Student <sreypichmen364@gmail.com>
2024-06-11 17:18:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from "react";
+import logo from './wreath.png';
+import DarkMode from './components/DarkMode/DarkMode';
+import { useTranslation } from "react-i18next";
 import './App.css';
 
 function App() {
+  const [t, i18n] = useTranslation("global");
+
+  const handleChangeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className='header-left'>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div>
+            <p>Vindica</p>
+          </div>
+        </div>
+        <div className='header-right'>
+          <div className="select-wrapper">
+            <select onChange={(e) => handleChangeLanguage(e.target.value)}>
+              <option value="en">EN</option>
+              <option value="fr">FR</option>
+            </select>
+           
+          </div>
+          <div> <DarkMode /></div>
+        </div>
+      </div>
+      <div className="banner">
+        <div className="banner_infor">
+          <h1>{t("header.message")}</h1>
+          <p>{t("home.body")}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Correct import statement
 import './index.css';
 import App from './App';
+import global_en from "./components/Translation/en/global.json";
+import global_fr from "./components/Translation/fr/global.json";
 import reportWebVitals from './reportWebVitals';
+import i18next from "i18next";
+import { I18nextProvider } from 'react-i18next';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en", 
+  resources: {
+    en: {
+      global: global_en 
+    },
+    fr: {
+      global: global_fr
+    }
+  }
+});
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
